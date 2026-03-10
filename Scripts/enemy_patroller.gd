eb3921b8143ff021051209bf37732a45758bf90c
Brendan Gasparin <brendan.gasparin@gmail.com>
2026-03-11 01:05:28 +1100

    Implement collision detection for birb and player

--- Scripts/enemy_patroller.gd
+++ Scripts/enemy_patroller.gd
@@ -7,10 +7,17 @@ extends Area2D
 @onready var target_pos : Vector2 = global_position + move_direction
 
 func _physics_process(delta):
+	# Patrol
 	global_position = global_position.move_toward(target_pos, move_speed * delta)
-	
 	if global_position == target_pos:
 		if target_pos == start_pos:
 			target_pos = start_pos + move_direction
 		else:
 			target_pos = start_pos
+
+func _on_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("Player"):
+		return
+	
+	# TODO: Deal damage to player
+	print("Deal damage to player.")
