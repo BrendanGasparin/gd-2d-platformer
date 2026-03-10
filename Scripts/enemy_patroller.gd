1ea8a0699f49c7725f67e927be9b445c5dc45e2d
Brendan Gasparin <brendan.gasparin@gmail.com>
2026-03-11 01:18:25 +1100

    Flip patroller sprite when moving left

--- Scripts/enemy_patroller.gd
+++ Scripts/enemy_patroller.gd
@@ -5,6 +5,10 @@ extends Area2D
 
 @onready var start_pos : Vector2 = global_position
 @onready var target_pos : Vector2 = global_position + move_direction
+@onready var sprite : Sprite2D = $Sprite
+
+func _ready():
+	$AnimationPlayer.play("walk")
 
 func _physics_process(delta):
 	# Patrol
@@ -14,6 +18,10 @@ func _physics_process(delta):
 			target_pos = start_pos + move_direction
 		else:
 			target_pos = start_pos
+	if start_pos.x > target_pos.x:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("Player"):
