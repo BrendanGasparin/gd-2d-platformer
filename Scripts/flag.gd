e734dd9c9dfa59fe61ad8f2caa20ae863007e697
Brendan Gasparin <brendan.gasparin@gmail.com>
2026-03-11 19:55:11 +1100

    Fix errors when player hits flag

--- Scripts/flag.gd
+++ Scripts/flag.gd
@@ -5,4 +5,7 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if not body.is_in_group("Player"):
 		return
+	call_deferred("_next_scene")
+
+func _next_scene():
 	get_tree().change_scene_to_file(next_scene)
