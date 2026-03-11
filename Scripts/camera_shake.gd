9b978fe802a49bc6eb041a6e40319e4d447f7874
Brendan Gasparin <brendan.gasparin@gmail.com>
2026-03-11 21:17:25 +1100

    Add camera shake when player takes damage

---
+++ Scripts/camera_shake.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+var intensity : float = 0.0
+
+func _ready():
+	get_parent().OnUpdateHealth.connect(_damage_shake)
+
+func _damage_shake(health: int):
+	intensity = 10
+
+func _process(delta):
+	if intensity > 0:
+		# Decrease intensity of shake to zero over time
+		intensity = lerpf(intensity, 0, delta * 10)
+		offset = _get_random_offset()
+
+func _get_random_offset():
+	var x = randf_range(-intensity, intensity)
+	var y = randf_range(-intensity, intensity)
+	return Vector2(x, y)
